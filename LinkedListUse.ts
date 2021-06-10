ab1d2586a367c69eaa1e24f27828f64dc953eb74
Ross O'Reilly <oreillyross@gmail.com>
2021-06-10 16:24:45 +0200

    Add linked list implementation

---
+++ LinkedListUse.ts
@@ -0,0 +1,14 @@
+import { LinkedList } from "./src/LinkedList";
+
+interface Person {
+  name: string;
+}
+
+const linkedList = new LinkedList<Person>();
+linkedList.insertAtEnd({ name: "Bob" });
+linkedList.insertInBegin({ name: "John" });
+const list = linkedList.traverse();
+console.log(list);
+const found = linkedList.search(({ name }) => name === "Bob");
+console.log(found);
+export const usage = "empty string"
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 import "./netflix"
+import { usage } from "./LinkedListUse"
